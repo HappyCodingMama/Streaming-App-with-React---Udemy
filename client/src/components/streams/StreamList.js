4381e4efbee3055c398e00ebcdfc86a57df1b1e2
HappyCodingMama <happycodingmama@gmail.com>
2021-01-30 22:52:01 +0900

    WildCard Navigation

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -12,7 +12,9 @@ class StreamList extends React.Component {
     if (stream.userId === this.props.currentUserId) {
       return (
         <div className='right floated content'>
-          <button className='ui button primary'>Edit</button>
+          <Link to={`/streams/edit/${stream.id}`} className='ui button primary'>
+            Edit
+          </Link>
           <button className='ui button negative'>Delete</button>
         </div>
       );
@@ -20,7 +22,7 @@ class StreamList extends React.Component {
   }
 
   renderList() {
-    return this.props.streams.map((stream) => {
+    return this.props.streams.map(stream => {
       return (
         <div className='item' key={stream.id}>
           {this.renderAdmin(stream)}
@@ -57,7 +59,7 @@ class StreamList extends React.Component {
   }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     streams: Object.values(state.streams),
     currentUserId: state.auth.userId,
